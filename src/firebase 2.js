4ea4ec66657472e60a43f3ce1618d7ce566b838d
Jereme Allen <jereme.allen@wheniwork.com>
2017-04-15 10:05:53 -0500

    Initial commit

--- src/firebase 2.js
+++ src/firebase 2.js
@@ -14,5 +14,3 @@ firebase.initializeApp(config);
 export default firebase;
 
 export const database = firebase.database();
-export const auth = firebase.auth();
-export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
